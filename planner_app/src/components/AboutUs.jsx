12791879cea3d1e490bc2b808ad9ee2eb4c473a2
MishaMazniak <Mishamaznyak@gmail.com>
2023-10-12 17:16:22 +0200

    corect class name

--- planner_app/src/components/AboutUs.jsx
+++ planner_app/src/components/AboutUs.jsx
@@ -2,13 +2,13 @@ function AboutUs() {
   return (
     <div>
       <h1>This is a planner app where you can:</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Add a task for a certain date</li>
-        <li class="list-group-item">Edit a previously added task</li>
-        <li class="list-group-item">Remove a task from my plan</li>
-        <li class="list-group-item">See all tasks for the selected day</li>
-        <li class="list-group-item">Move a task from one day to another</li>
-        <li class="list-group-item">Mark the task as completed</li>
+      <ul className="list-group">
+        <li className="list-group-item">Add a task for a certain date</li>
+        <li className="list-group-item">Edit a previously added task</li>
+        <li className="list-group-item">Remove a task from my plan</li>
+        <li className="list-group-item">See all tasks for the selected day</li>
+        <li className="list-group-item">Move a task from one day to another</li>
+        <li className="list-group-item">Mark the task as completed</li>
       </ul>
     </div>
   )
